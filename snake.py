27f6dcf5a8b07bc99488444337112422c80dcb20
helinskij <101726504+helinskij@users.noreply.github.com>
2026-01-13 10:48:05 -0500

    Added more variants of food

--- snake.py
+++ snake.py
@@ -1,53 +1,25 @@
-import time
-from turtle import Turtle, Screen
+from turtle import Turtle
+import random
 
-STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
-LEFT = 180
-RIGHT = 0
+SHAPES = ['circle', 'square', 'triangle']
+COLORS = ['green', 'blue', 'red', 'yellow', 'purple']
 
-class Snake:
+class Food(Turtle):
     def __init__(self):
-        self.snake = []
-        self.create_snake()
-        self.head = self.snake[0]
+        super().__init__()
+        self.shape(random.choice(SHAPES))
+        self.penup()
+        self.shapesize(stretch_len=0.6, stretch_wid=0.6)
+        self.color(random.choice(COLORS))
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x,random_y)
+        self.refresh()
 
-    def create_snake(self):
-        for position in STARTING_POSITIONS:
-            self.add_segment(position)
-
-    def add_segment(self, position):
-        segment = Turtle(shape="square")
-        segment.color("white")
-        segment.penup()
-        segment.goto(position)
-        self.snake.append(segment)
-
-    def extend(self):
-        self.add_segment(self.snake[-1].position())
-
-    def move(self):
-        for seg_num in range(len(self.snake) - 1, 0, -1):
-            new_x = self.snake[seg_num - 1].xcor()
-            new_y = self.snake[seg_num - 1].ycor()
-            self.snake[seg_num].goto(new_x, new_y)
-        self.snake[0].forward(MOVE_DISTANCE)
-
-    def up(self):
-        if self.head.heading()!=DOWN:
-            self.head.setheading(UP)
-
-    def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
-
-    def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
-
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
-
+    def refresh(self):
+        self.color(random.choice(COLORS))
+        self.shape(random.choice(SHAPES))
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
