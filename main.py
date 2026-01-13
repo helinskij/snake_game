f38547ae2b08deadd453ea2f31e82d4343d0c0d9
helinskij <101726504+helinskij@users.noreply.github.com>
2026-01-13 10:46:46 -0500

    Refactor game over handling in main.py
    Updated game over logic to reset the scoreboard and snake.

--- main.py
+++ main.py
@@ -36,14 +36,14 @@ while game_is_on:
 
     #Detecting collision with wall
     if snake.head.xcor()>280 or snake.head.xcor()<-290 or snake.head.ycor()>280 or snake.head.ycor() <-280:
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     #Detecting collision with tail
     for segment in snake.snake[1:]:
         if snake.head.distance(segment)<10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
