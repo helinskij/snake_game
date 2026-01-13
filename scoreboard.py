cdb2ddd4044c7e5fca20688631b793381e44517c
helinskij <101726504+helinskij@users.noreply.github.com>
2026-01-13 10:47:19 -0500

    Added high score saved in txt file

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 from food import Turtle
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
+        with open("high_score.txt", mode="r") as file:
+            self.highscore = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -12,7 +16,8 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align="center", font=("Arial", 20, "normal"))
 
 
     def add_point(self):
@@ -20,9 +25,14 @@ class Scoreboard(Turtle):
         self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align="center", font=("Arial", 42, "normal"))
+    def reset(self):
+        if self.score>self.highscore:
+            self.highscore = self.score
+            with open("high_score.txt", mode="w") as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
+
 
 
 
